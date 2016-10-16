d70f911d83e2ad3b501aef432ef5745d6b6ae5de
Geoff Simons <geoff.simons@gmail.com>
2016-10-16 15:12:57 -0700

    Cleaned out old testing code
    Moving out code that is no longer needed.

--- http.js
+++ http.js
@@ -1,3 +1,8 @@
+/**
+  This is a little wrapper to load up main.js as a
+  port 80 HTTP server.
+*/
+
 var express = require('express');
 var router = require('./main');
 
@@ -5,15 +10,9 @@ var app = express();
 
 app.use('/', router);
 
-// var mongo = require('./mongostore');
-
-// mongo.init(function(res) {
-
 var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
-// });
