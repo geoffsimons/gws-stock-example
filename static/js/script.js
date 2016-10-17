7837bc407877a8864daf6eb66c22366456c11360
Geoff Simons <geoff.simons@gmail.com>
2016-10-16 20:11:25 -0700

    Primarily a fix for redundant lookup calls.
    Also added notes about implementing up and down arrows
    to navigate the suggestions.

--- static/js/script.js
+++ static/js/script.js
@@ -12,9 +12,14 @@ function lookup() {
     return;
   }
   cancelLookup(); //Will check if acTimeout is truthy
+  acInput = q;
   if(q.length > 0) {
     acTimeout = setTimeout(fetchLookup,300,q);
-    acInput = q;
+  }
+  else {
+    //If the user cleared the input,
+    //clear the suggestions.
+    $('#suggestions').hide();
   }
 }
 
@@ -24,7 +29,6 @@ function cancelLookup() {
     clearTimeout(acTimeout);
   }
   acTimeout = null;
-  acInput   = null;
 }
 
 function fetchLookup(q) {
@@ -38,16 +42,28 @@ function fetchLookup(q) {
 function showSuggestions(sugs) {
   //TODO: Assert that sugs is an array, silently fail.
   console.log("showSuggestions()... acInput:",acInput);
-  if(!acInput) {
-    console.log("Suggestions were canceled. acInput not valid.");
-    //Do we need to do any dom element cleanup?
+  if(!acTimeout) {
+    console.log("Suggestions were canceled. acTimeout not valid.");
     return;
   }
+
+  //TODO: Cache the sugs object to enable arrow UP and DOWN nav.
+
   //NOTE: The API does not return our input, so there is no
   //      way to guarantee that sugs match, but we do cancel
   //      fetches in order to save hits on any rate limit,
   //      which in turn means there will be less sugs coming
   //      through this function.
+
+  //NOTE: The API may be rate limiting our lookups.
+  //      After making a few quick lookups, the requests
+  //      started to hang. Not sure if this is a Chrome/MacOS
+  //      issue, of if it is hanging on the server side (seems
+  //      like a weird way to rate limit), or if jQuery or the
+  //      underlying browser are maybe not canceling enough
+  //      requests.
+  //   We can try to change how long we wait to do a lookup
+  //   after a change to the input field, but not sure if that would help.
 
   // console.log(sugs);
   var $list = $('#suggestions');
@@ -173,17 +189,32 @@ $(document).ready(function () {
   });
 
   $('#search').mouseup(function(e) {
-    $(this).blur();
+    $(this).blur(); //Possibly just for Chrome/MacOS
   });
 
   $('#symbol').keyup(function(e) {
+    // console.log(e.which);
     if(e.which == 13) {
+      //TODO: if(symFocus) $(this).val(symFocus);
+      //  Assuming that symFocus == highlighted row's symbol.
       startSearch();
     }
     //TODO: else if e.which is a UP or DOWN arrow
     //      We should move the highlight.
-    //      Also, if a row is highlighted, use that
-    //      for the search instead of the input value.
+    //      In order to implement, we need to have a
+    //      way to get the list of suggestions,
+    //      so instead of passing that param from the
+    //      ajax response, we could set it to a var.
+    //      Then we would just need to store the index.
+    //      -1 < sugIndex < sugs.length
+    if(e.which == 38) { //UP
+      //Up is actually moving "back" in the array.
+      //TODO: sugIndex = max(sugIndex-1, -1);
+    }
+    if(e.which == 40) { //DOWN
+      //Down is actually moving "forward" in the array.
+      //TODO: sugIndex = min(sugIndex+1, sugs.length-1);
+    }
     else {
       //TODO: convert the query to upper-case.
       lookup();
